c1824ade9809f56710f87827493eea012cb42599
Chris Rice <chrisrice2442@yahoo.com>
2023-12-11 15:17:51 -0500

    post route added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
-const {v4: uuidv4} = require('uuid');
+const {v4: uuid} = require('uuid');
 const fs = requirer('fs');
 
 //get route
-router.get('/api.notes', async (req, res) => {
+router.get('/api/notes', async (req, res) => {
     const dbJson = await JSON.parse(fs.readFileSync('db/db.json', 'utf8'))
     res.json(dbJson);
 })
 
+//post route
+router.post('/api/notes', (req, res) => {
+    const dbJson = JSON.parse(fs.readFileSync('db/db.json','utf8'))
+    const newFeedback = {
+        title: req.body.title,
+        text: req.body.text, 
+        id: uuid(),
+    };
+    dbJson.push(newFeedback)
+    fs.writeFileSync('db/db.json', JSON.stringify(dbJson))
+    res.json(dbJson)
+})
